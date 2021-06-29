44e7a806740081b3dbcbe0b5a286d8ad92c0243a
小池翔 <shaw@koikeshounoMacBook-Pro.local>
2021-06-29 23:30:50 +0900

    QA

--- RecordShohinPrice/src/main/java/com/example/demo/service/CategoryService.java
+++ RecordShohinPrice/src/main/java/com/example/demo/service/CategoryService.java
@@ -5,16 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.CategoryDao;
 import com.example.demo.entity.CategoryEntity;
-import com.example.demo.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
 
 	@Autowired
-	CategoryRepository repository;
-	
+	CategoryDao categoryDao;
+
 	public List<CategoryEntity> findAll() {
-		return repository.findAll();
+		return categoryDao.findAll();
 	}
 }
--- RecordShohinPrice/src/main/java/com/example/demo/service/ShopService.java
+++ RecordShohinPrice/src/main/java/com/example/demo/service/ShopService.java
@@ -5,17 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.ShopDao;
 import com.example.demo.entity.ShopEntity;
-import com.example.demo.repository.ShopRepository;
 
 @Service
 public class ShopService {
-	
+
 	@Autowired
-	ShopRepository repository;
-	
+	ShopDao shopDao;
+
 	public List<ShopEntity> findAll() {
-		return repository.findAll();
+		return shopDao.findAll();
 	}
-	
+
 }
