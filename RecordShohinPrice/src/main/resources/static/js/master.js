b41db9dfd0ba16a3289c823e3780ecd1aa83c757
mtdmnok <yamamotonaoko@hotmail.co.jp>
2021-10-31 23:54:44 +0900

    20211031 マスタ画面

--- RecordShohinPrice/src/main/resources/static/js/master.js
+++ RecordShohinPrice/src/main/resources/static/js/master.js
@@ -24,70 +24,123 @@ $(function(){
 
 
 // 【　クリックしたテーブルのセルを編集可能にする　】
-
-
-$(function($, document) {
-	  $.fn.tablecelleditor = function(options) {
-	    var settings = $.extend(true, {}, $.fn.tablecelleditor.defaults, options);
-	    return $(this).each(function() {
-	      $(this).dblclick(function() {
-	        $.fn.tablecelleditor.startEdit($(this), settings);
-	      });
-	    });
-	  };
-	  $.fn.tablecelleditor.defaults = {
-	    inputCSS: {
-	      border: 0,
-	      width: "100%"
-	    }
-	  };​
-	  $.fn.tablecelleditor.startEdit = function(td, settings) {
-	    var originalContent = td.text();
-	    td.addClass("cellEditing");
-	    td.data("originalContent", originalContent);
+$(function() {
+	$("td").dblclick(function() {
+		var td = $(this), originalContent = td.text();
+		
+		td.addClass("cellEditing");
+		td.data("originalContent", originalContent);
 	    var el = document.createElement("input"), $el = $(el);
 	    $el.attr({type: "text", value: originalContent});
 	    $el.css("fontSize", td.css("fontSize"));
-	    $el.css(settings.inputCSS);
 	    td.empty();
 	    td.append(el);
-	    $el.focus();​
+	    $el.focus();
+	    
 	    $el.keypress(function(e) {
-	      if (e.which == 13) {
-	        $.fn.tablecelleditor.commitEdit(e, settings);
-	      }
+	        if (e.which == 13) {
+	          var text = $(this), newContent = text.val(), td = text.parent();
+	          td.text(newContent);
+	          td.removeData("originalContent");
+	          td.removeClass("cellEditing");
+	        }
 	    });
-	    $el.keydown(function(e) {
-	      if (e.which == 27) {
-	        $.fn.tablecelleditor.resetEdit(e, settings);
-	      }
-	    });
-	    $el.blur(function(e) {
-	      $.fn.tablecelleditor.resetEdit(e, settings);
-	    });
-	  };​
-	  $.fn.tablecelleditor.commitEdit = function(e) {
-	    var text = $(e.target), newContent = text.val(), td = text.parent();
-	    td.text(newContent);
-	    td.removeData("originalContent");
-	    td.removeClass("cellEditing");
-	  };
-	  $.fn.tablecelleditor.resetEdit = function(e) {
-	    var td = $(e.target).parent();
-	    td.text(td.data("originalContent"));
-	    td.removeData("originalContent");
-	    td.removeClass("cellEditing");
-	  };
-	}(jQuery, document));
+	    var resetContent = function(e) {
+	        var td = $(e.target).parent();
+	        td.text(td.data("originalContent"));
+	        td.removeData("originalContent");
+	        td.removeClass("cellEditing");
+	      };
 
+	      $el.keydown(function(e) {
+	        if (e.which == 27) {
+	          resetContent(e);
+	        }
+	      });
 
-$(function() {
-	  $("td").tablecelleditor({
-	    inputCSS: {
-	      "background-color": "rgb(255,253,210)"
-	    }
-	  });
+	      $el.blur(resetContent);
 	});
+});
+
+
+
+
+
+
+//
+//$(function($, document) {
+//	  $.fn.tablecelleditor = function(options) {
+//	    var settings = $.extend(true, {}, $.fn.tablecelleditor.defaults, options);
+//	    return $(this).each(function() {
+//	      $(this).dblclick(function() {
+//	        $.fn.tablecelleditor.startEdit($(this), settings);
+//	      });
+//	    });
+//	  };
+//	  $.fn.tablecelleditor.defaults = function(){
+//	    inputCSS: {
+//	      border: 0;
+//	      width: "100%";
+//	    }
+//	  };​
+//	  $.fn.tablecelleditor.startEdit = function(td, settings) {
+//	    var originalContent = td.text();
+//	    td.addClass("cellEditing");
+//	    td.data("originalContent", originalContent);
+//	    var el = document.createElement("input"), $el = $(el);
+//	    $el.attr({type: "text", value: originalContent});
+//	    $el.css("fontSize", td.css("fontSize"));
+//	    $el.css(settings.inputCSS);
+//	    td.empty();
+//	    td.append(el);
+//	    $el.focus();​
+//	    $el.keypress(function(e) {
+//	      if (e.which == 13) {
+//	        $.fn.tablecelleditor.commitEdit(e, settings);
+//	      }
+//	    });
+//	    $el.keydown(function(e) {
+//	      if (e.which == 27) {
+//	        $.fn.tablecelleditor.resetEdit(e, settings);
+//	      }
+//	    });
+//	    $el.blur(function(e) {
+//	      $.fn.tablecelleditor.resetEdit(e, settings);
+//	    });
+//	  };​
+//	  $.fn.tablecelleditor.commitEdit = function(e) {
+//	    var text = $(e.target), newContent = text.val(), td = text.parent();
+//	    td.text(newContent);
+//	    td.removeData("originalContent");
+//	    td.removeClass("cellEditing");
+//	  };
+//	  $.fn.tablecelleditor.resetEdit = function(e) {
+//	    var td = $(e.target).parent();
+//	    td.text(td.data("originalContent"));
+//	    td.removeData("originalContent");
+//	    td.removeClass("cellEditing");
+//	  };
+//	}(jQuery, document));
+//
+//
+//$(function() {
+//	  $("td").tablecelleditor({
+//	    inputCSS: {
+//	      "background-color": "rgb(255,253,210)"
+//	    }
+//	  });
+//	});
+
+
+
+
+
+
+
+
+
+
+
 //// これだと、テーブルをクリックしたときに反応する。しかし、セルの選択はできない。
 //$(function(){
 //	var tt = document.getElementById("resistTable");
